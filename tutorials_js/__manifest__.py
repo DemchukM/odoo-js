9ad7d8a6fd67634da339b632705ada59348312ac
Demchuk Mukola <kolya233445@gmail.com>
2023-01-20 21:48:24 +0200

    [ADD]tutorials_js: new module

---
+++ tutorials_js/__manifest__.py
@@ -0,0 +1,45 @@
+{
+    'name': "Tutorials js",
+
+    'summary': """
+        Play with Owl in this playground module""",
+
+    'description': """
+        Play with Owl in this playground module
+    """,
+
+    'author': "DemchukM",
+
+    'category': 'Productivity',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web'],
+    'application': True,
+    'installable': True,
+    'data': [
+        'views/templates.xml',
+    ],
+    'assets': {
+        'tutorials_js.my_test': [
+            # bootstrap
+            ('include', 'web._assets_helpers'),
+            'web/static/src/scss/pre_variables.scss',
+            'web/static/lib/bootstrap/scss/_variables.scss',
+            ('include', 'web._assets_bootstrap'),
+
+            'web/static/src/libs/fontawesome/css/font-awesome.css', # required for fa icons
+            'web/static/src/legacy/js/promise_extension.js', # required by boot.js
+            'web/static/src/boot.js', # odoo module system
+            'web/static/src/env.js', # required for services
+            'web/static/src/session.js', # expose __session_info__ containing server information
+            'web/static/lib/owl/owl.js', # owl library
+            'web/static/lib/owl/odoo_module.js', # to be able to import "@odoo/owl"
+            'web/static/src/core/utils/functions.js',
+            'web/static/src/core/browser/browser.js',
+            'web/static/src/core/registry.js',
+            'web/static/src/core/assets.js',
+            'tutorials_js/static/src/**/*',
+        ],
+    }
+}
